424cbf49ee6358f7359218b988122f21dd7c6a83
WHWONE <whwone@gmail.com>
2026-02-12 19:05:43 -0600

    Delete src/ui/dom.js

--- src/ui/dom.js
+++
@@ -1,9 +0,0 @@
-export const $ = (id) => document.getElementById(id);
-
-export function getPianoEl() {
-  return $('piano');
-}
-
-export function getMelodyDisplay() {
-  return $('melody-display');
-}
